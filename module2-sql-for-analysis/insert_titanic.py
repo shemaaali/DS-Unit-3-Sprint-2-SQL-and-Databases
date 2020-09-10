b145e9cc1cad898ee7b5d08fde92b1ba450a4eaf
shemaaali <shemaali8787@gmail.com>
2020-09-09 23:08:59 -0500

    Added for loop to insert table

--- module2-sql-for-analysis/insert_titanic.py
+++ module2-sql-for-analysis/insert_titanic.py
@@ -2,58 +2,51 @@ import pandas as pd
 import numpy as np
 import psycopg2
 
-DATA_PATH  = 'https://raw.githubusercontent.com/LambdaSchool/DS-Unit-2-Linear-Models/master/data/'
-df = pd.read_csv(DATA_PATH+'titanic/train.csv')
+df = pd.read_csv('titanic.csv')
 print(df.shape)
 print(df.head())
-
 
 DB_NAME = 'obgeebcm'
 DB_USER = 'obgeebcm'
 DB_PASSWORD = 'V8UxJY7QEPU20Tbojx_2fkSFq1KdLT7O'
-DB_HOST = 'lallah.db.elephantsql'
+DB_HOST = 'lallah.db.elephantsql.com'
 
 conn = psycopg2.connect(dbname=DB_NAME, user=DB_USER,
                         password=DB_PASSWORD, host = DB_HOST)
-
-create_titanic = """
-CREATE TABLE titanic_table (
-  PassengerId       SERIAL PRIMARY KEY,
-  Survived          int,
-  Pclass            int,
-  Name	            varchar(40) NOT NULL,
-  Sex               varchar(40) NOT NULL,	
-  Age               int,
-  SibSp             int,
-  Parch	            int,
-  Ticket            varchar(40) NOT NULL,
-  Fare              int,
-  Cabin             varchar(40) NOT NULL,
-  Embarked          varchar(40) NOT NULL,
-  data    JSONB
-);
-"""
-
-insert_titanic = """
-INSERT INTO titanic_table(PassengerId, Survived, Pclass ,Name, Sex, Age, SibSp, Parch, Ticket data) VALUES
-(
-  'A row Name',
-  null
-),
-(
-  'Another row, with JSON',
-  '{ "S": 3, "NaN": ["C85", "C123",  53.1000], "C": true }'::JSONB
-);
-"""
-
+                   
 print("connection:", conn)
 cur = conn.cursor()
 print("cursor:", cur)
 
-cur.execute(create_titanic)
-cur.execute(insert_titanic)
+cur.execute('''DROP TABLE IF EXISTS titanic_table; CREATE TABLE IF NOT EXISTS titanic_table(
+  Survived bytea,
+  Pclass smallint,
+  Name varchar(200),
+  Sex sex_binary	
+  Age decimal,
+  Siblings_Spouses_Aboard smallint,
+  Parent_Children_Aboard smallint,
+  Fare decimal);''')
 
-cur.execute("SELECT * from create_titanic;")
+#cur.execute('''INSERT INTO titanic_table(Survived, Pclass ,Name, Sex, Age, SibSp, Parch, Ticket, Fare, Cabin, Embarked)
+#VALUES 
+#(2, 3, 'C8556', 'C85', 23,  53.1000, 67, 'good', 12, 'btyu', 'auto'),
+#(9, 7, 'C7867', 'C67', 125,  55.1000, 5, 'goodness', 13, 'niceo', 'beyoon');''')
+
+cur.execute("SELECT * from titanic_table;")
 
 result_query = cur.fetchall()
-print(result_query)
+print("result_query:",result_query)
+
+#cur.close()
+#conn.commit()
+#conn.close()
+
+# for loop to insert the whole table
+for index, row in df.iterrows():
+     cur.execute(f'''INSERT INTO titanic_table(Survived, Pclass ,Name, Sex, Age, Siblings_Spouses_Aboard, Parent_Children_Aboard,	Fare)
+VALUES 
+( 
+  {row[0]}, {row[1]}, '{row[2]}', '{row[3]}', '{row[4]}', {row[5]}, {row[6]}, {row[7]}
+)
+    ''')
